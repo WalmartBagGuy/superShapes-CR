92c2450016910a772ad365d088b26a3ad099fb89
Walegi4 <130806226+MinnieMia@users.noreply.github.com>
2023-09-19 17:42:30 -0500

    initalize

---
+++ ～(　TロT)σ/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Default Movement Speed")]
+    publc float moveSpeed = 600f;
+    float movement = 0f
+    void Start()
+    {
+     Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     movement = Input.GetAxisRaw("Horizontal");
+    }
+}
